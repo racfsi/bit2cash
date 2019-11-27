6026c1d182569fa3afe473471d4af6dda2096a0a
racfsi <ruizmunozc@gmail.com>
2019-11-26 22:35:25 -0500

    Tabs infobit

--- src/components/infobit/infobit-component.js
+++ src/components/infobit/infobit-component.js
@@ -1,44 +1,79 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 /*ESTILOS*/
 import "./infobit-component.css";
 
 class BitInfo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      bitName: "",
-      bitPrice: "",
-      bitImage: ""
-    };
-  }
   render() {
     return (
       <div class="divRightHeaderHome">
         <div class="chartsImgToptHeaderHome"></div>
         <div class="infoDivHeaderHome">
-          <ul>
-            <li>COP</li>
-            <li class="active">USD</li>
-            <li>EUR</li>
-          </ul>
-          <div class="infoDivDetHeaderHome">
-            <h3>BTC</h3>
-            <p class="infoDivDetPrHeaderHome">9,437.05 USD</p>
-            <div class="chartsHeaderHome">
-              <div class="chartsImgHeaderHome">
-                <img
-                  src={require("../../assets/images/home/chart-graphics.png")}
-                ></img>
+          <Tabs>
+            <TabList>
+              <Tab>COP</Tab>
+              <Tab>USD</Tab>
+              <Tab>EUR</Tab>
+            </TabList>
+            <TabPanel>
+              <div class="infoDivDetHeaderHome">
+                <h3>COP</h3>
+                <p class="infoDivDetPrHeaderHome">10,890.05 COP</p>
+                <div class="chartsHeaderHome">
+                  <div class="chartsImgHeaderHome">
+                    <img
+                      src={require("../../assets/images/home/chart-graphics.png")}
+                    ></img>
+                  </div>
+                  <p>-1.01%</p>
+                </div>
+                <div class="chartsImgFootHeaderHome">
+                  <img
+                    src={require("../../assets/images/home/chart-bottom.png")}
+                  ></img>
+                </div>
               </div>
-              <p>-1.01%</p>
-            </div>
-            <div class="chartsImgFootHeaderHome">
-              <img
-                src={require("../../assets/images/home/chart-bottom.png")}
-              ></img>
-            </div>
-          </div>
+            </TabPanel>
+            <TabPanel>
+              <div class="infoDivDetHeaderHome">
+                <h3>USD</h3>
+                <p class="infoDivDetPrHeaderHome">9,437.05 USD</p>
+                <div class="chartsHeaderHome">
+                  <div class="chartsImgHeaderHome">
+                    <img
+                      src={require("../../assets/images/home/chart-graphics.png")}
+                    ></img>
+                  </div>
+                  <p>-1.01%</p>
+                </div>
+                <div class="chartsImgFootHeaderHome">
+                  <img
+                    src={require("../../assets/images/home/chart-bottom.png")}
+                  ></img>
+                </div>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div class="infoDivDetHeaderHome">
+                <h3>EUR</h3>
+                <p class="infoDivDetPrHeaderHome">8,365.05 USD</p>
+                <div class="chartsHeaderHome">
+                  <div class="chartsImgHeaderHome">
+                    <img
+                      src={require("../../assets/images/home/chart-graphics.png")}
+                    ></img>
+                  </div>
+                  <p>-1.01%</p>
+                </div>
+                <div class="chartsImgFootHeaderHome">
+                  <img
+                    src={require("../../assets/images/home/chart-bottom.png")}
+                  ></img>
+                </div>
+              </div>
+            </TabPanel>
+          </Tabs>
         </div>
       </div>
     );
